d959c6fb601ca5c606bb30490e19de92a16f419e
Nishat <nishat@empireone.com.au>
2013-10-15 13:43:48 +1100

    added plus and minus gotcha types.

---
+++ gotchas/minus_gotcha.rb
@@ -0,0 +1,29 @@
+class MinusGotcha < Gotcha::Base
+
+  DEFAULT_MIN = 0
+  DEFAULT_MAX = 20
+
+  def initialize
+    rand1 = self.class.random_number_in_range
+    rand2 = self.class.random_number_in_range
+    @question = ["#{rand1} - #{rand2} = ", "#{rand1} minus #{rand2} equals ", "#{rand1.to_words} minus #{rand2.to_words} equals ", "#{rand1} minus #{rand2.to_words} equals ", "#{rand1.to_words} minus #{rand2} equals "][rand(5)]
+    @answer = rand1 - rand2
+  end
+
+  private
+
+  def self.max
+    @@max ||= DEFAULT_MAX
+  end
+
+  def self.min
+    @@min ||= DEFAULT_MIN
+  end
+
+  def self.random_number_in_range
+    rand(self.max - self.min) + self.min
+  end
+
+end
+
+Gotcha.register_type MinusGotcha
